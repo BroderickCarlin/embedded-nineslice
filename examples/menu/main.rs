6d0819a83d10496fc5897f0aa3b9c0bf7e2eeaf6
Broderick Carlin <broderick.carlin@gmail.com>
2023-11-14 13:22:08 -0600

    feat: Update example to be more compelling

--- examples/menu/main.rs
+++ examples/menu/main.rs
@@ -1,6 +1,6 @@
 use embedded_graphics::{
     image::Image,
-    mono_font::{ascii::FONT_10X20, MonoTextStyleBuilder},
+    mono_font::{ascii::FONT_7X13, MonoTextStyleBuilder},
     pixelcolor::Rgb888,
     prelude::*,
     primitives::{PrimitiveStyle, Rectangle},
@@ -12,51 +12,64 @@ use tinybmp::Bmp;
 
 const BORDER_IMAGE_RAW: &[u8] = include_bytes!("border.bmp");
 
+fn draw_text<D: DrawTarget<Color = Rgb888>>(text: &str, position: Point, display: &mut D) {
+    let character_style = MonoTextStyleBuilder::new()
+        .font(&FONT_7X13)
+        .text_color(Rgb888::CSS_BLACK)
+        .build();
+
+    let center_aligned = TextStyleBuilder::new()
+        .alignment(Alignment::Left)
+        .baseline(Baseline::Middle)
+        .build();
+
+    let _ = Text::with_text_style(text, position, character_style, center_aligned).draw(display);
+}
+
+fn draw_background<C: PixelColor, D: DrawTarget<Color = C> + OriginDimensions>(
+    color: C,
+    display: &mut D,
+) {
+    let background_fill = PrimitiveStyle::with_fill(color);
+
+    let _ = Rectangle::new(Point::zero(), display.size())
+        .into_styled(background_fill)
+        .draw(display);
+}
+
 fn main() {
     let display_resolution = Size::new(160, 144);
     let mut display: SimulatorDisplay<Rgb888> = SimulatorDisplay::new(display_resolution);
+    draw_background(Rgb888::new(255, 255, 255), &mut display);
+
     let border_bmp = Bmp::from_slice(BORDER_IMAGE_RAW).unwrap();
 
     let cfg = NineSliceConfig {
-        size: Size::new(160, 30),
-        left_width: 7,
-        top_height: 7,
-        right_width: 7,
-        bottom_height: 7,
+        size: Size::new(80, 128),
+        left_width: 8,
+        top_height: 8,
+        right_width: 8,
+        bottom_height: 8,
         fill_center: false,
     };
 
     let border_nineslice = NineSlice::new(&border_bmp, cfg);
 
-    let border_image = Image::new(&border_nineslice, Point::zero());
-
-    let background_fill = PrimitiveStyle::with_fill(Rgb888::new(255, 241, 232));
-
-    Rectangle::new(Point::zero(), display_resolution)
-        .into_styled(background_fill)
-        .draw(&mut display)
-        .unwrap();
+    let border_image = Image::new(&border_nineslice, Point::new(80, 0));
 
     border_image.draw(&mut display).unwrap();
 
-    let character_style = MonoTextStyleBuilder::new()
-        .font(&FONT_10X20)
-        .text_color(Rgb888::CSS_SLATE_GRAY)
-        .build();
+    let spacing = 15;
+    let mut offset = 20;
 
-    let center_aligned = TextStyleBuilder::new()
-        .alignment(Alignment::Center)
-        .baseline(Baseline::Middle)
-        .build();
+    let menu_items = &[
+        "POKeDEX", "POKeMON", "ITEM", "ASH", "SAVE", "OPTION", "EXIT",
+    ];
 
-    Text::with_text_style(
-        "Example",
-        Point::new(80, 15),
-        character_style,
-        center_aligned,
-    )
-    .draw(&mut display)
-    .unwrap();
+    for item in menu_items {
+        draw_text(item, Point::new(100, offset), &mut display);
+        offset += spacing
+    }
 
     let output_settings = OutputSettingsBuilder::new().scale(3).build();
     Window::new("BMP image", &output_settings).show_static(&display);
